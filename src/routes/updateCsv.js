1e10ec557e886b61d90cf6cddf4ba397194aeff0
Imsiaw <78824988+Imsiaw@users.noreply.github.com>
2023-07-30 00:52:07 +0330

    Update updateCsv.js

--- src/routes/updateCsv.js
+++ src/routes/updateCsv.js
@@ -1,34 +1,61 @@
-const fs = require("fs");
 const axios = require("axios");
-const { config } = require("./config/index.js");
+const fs = require("fs");
+const { updateLine } = require("../utils/updateLine.js");
 
-const { CSV_PATH, CSV_UPDATE_PATH } = config;
+const { config } = require("../config/index.js");
 
-fs.unlink(CSV_UPDATE_PATH, (err) => {
-  console.log(err);
-  const url =
-    "https://nc-aftermarket-www-production.s3.amazonaws.com/reports/Namecheap_Market_Sales.csv";
-  const writer = fs.createWriteStream(CSV_UPDATE_PATH);
-  console.log("Updating Csv File...");
-  axios({
-    url,
-    method: "GET",
-    responseType: "stream",
-  })
-    .then((resp) => {
-      console.log("Response Reached... \nStart Downloading");
-      resp.data.pipe(writer);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-  writer.on("finish", () => {
-    fs.rename(CSV_UPDATE_PATH, CSV_PATH, (_error) => {
-      if (_error) {
-        console.error(`Error : ${_error}`);
-        return;
+const updateCsv = async (req, res) => {
+  const { CSV_DOWNLOAD_URL, CSV_PATH, CSV_UPDATE_PATH } = config;
+
+  fs.unlink(CSV_UPDATE_PATH, (err) => {
+    console.log(err);
+    const writer = fs.createWriteStream(CSV_UPDATE_PATH);
+
+    let responseFinished = false;
+
+    res.on("close", () => {
+      if (!responseFinished) {
+        writer.destroy();
       }
-      console.log("Finish.");
+    });
+
+    axios({
+      url: CSV_DOWNLOAD_URL,
+      method: "GET",
+      responseType: "stream",
+    })
+      .then((resp) => {
+        const totalLen = resp.headers["content-length"];
+        let downloadLen = 0;
+        resp.data.on("data", (chunk) => {
+          downloadLen += chunk.length;
+          const progress = Math.round((downloadLen / totalLen) * 100);
+          updateLine(`${progress}%`);
+          if (!responseFinished) {
+            res.write(JSON.stringify({ progress }));
+          }
+        });
+        resp.data.pipe(writer);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error");
+      });
+    writer.on("finish", () => {
+      fs.rename(CSV_UPDATE_PATH, CSV_PATH, (_error) => {
+        if (_error) {
+          console.error(_error);
+        }
+        if (!responseFinished) {
+          res.end("Download finish");
+        }
+      });
+    });
+    res.on("finish", () => {
+      responseFinished = true;
     });
   });
-});
+  // await fsPromisify.unlink(CSV_UPDATE_PATH);
+};
+
+module.exports = { updateCsv };
