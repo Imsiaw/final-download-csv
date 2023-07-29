2063bf5f4088306c2276082a2ab3621ac31e2cd0
Imsiaw <78824988+Imsiaw@users.noreply.github.com>
2023-07-30 00:49:41 +0330

    Update lastUpdate.js

--- src/routes/lastUpdate.js
+++ src/routes/lastUpdate.js
@@ -1,11 +1,14 @@
-const fsPromisify = require("fs/promises");
+// const fsPromisify = require("fs/promises");
+const fs = require("fs");
 const { config } = require("../config/index.js");
 
 const lastUpdate = async (req, res) => {
   const { CSV_PATH } = config;
   try {
-    const stats = await fsPromisify.stat(CSV_PATH);
-    res.json({ lastModified: stats.mtime, error: false, message: "" });
+    fs.stat(CSV_PATH, (err, stats) => {
+      res.json({ lastModified: stats.mtime, error: false, message: "" });
+    });
+    // const stats = await fsPromisify.stat(CSV_PATH);
   } catch (err) {
     res.json({ error: true, message: err.message, data: null });
   }
